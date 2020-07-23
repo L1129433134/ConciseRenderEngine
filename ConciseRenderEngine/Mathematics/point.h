0fb7a0e6e9da7f7986357aaeda8e46daac90bc01
scmales <scmales@163.com>
2020-07-24 01:30:07 +0800

    operator==

--- ConciseRenderEngine/Mathematics/point.h
+++ ConciseRenderEngine/Mathematics/point.h
@@ -12,11 +12,14 @@ namespace scmales
         int m_dim;
 
     public:
-        Point(){m_data = new T[dim]};
+        Point()
+        {
+            m_data = new T[dim];
+        }
         Point(std::initializer_list<T> list)
         {
             m_dim = dim;
-            m_date = new T[dim];
+            m_data = new T[dim];
             int i = 0;
             for (auto it = list.begin(); it != list.end(); it++)
             {
@@ -26,14 +29,37 @@ namespace scmales
         Point(const Point &point) //复制构造函数
         {
             m_dim = dim;
-            m_date = new T[dim];
+            m_data = new T[dim];
             int i = 0;
-            for (int i = 0; i < m_count; i++)
+            for (int i = 0; i < m_dim; i++)
             {
                 *(m_data + i) = *(point.m_data + i);
             }
         }
-        ~Point(){};
+        
+        bool operator == (const Point &point)
+        {
+            return this->equal(point);
+        }
+        bool equal(const Point &point)
+        {
+            return Point::equal(*this, point);
+        }
+        static bool equal(const Point &a, const Point &b)
+        {
+            if (&a == &b)
+                return true;
+            if (a.m_dim != b.m_dim)
+                return false;
+
+            for (int i = 0; i < a.m_dim; i++)
+            {
+                if (*(a.m_data + i) != *(a.m_data + i))
+                    return false;
+            }
+            return true;
+        }
+        virtual ~Point(){};
     };
 } // namespace scmales
 #endif
--- Tests/ConciseRenderEngine/Mathematics/point_test.cpp
+++ Tests/ConciseRenderEngine/Mathematics/point_test.cpp
@@ -1,7 +1,16 @@
 #include "point.h"
 #include <iostream>
+#include <initializer_list>
 int main()
 {
-    std::cout<< "HelloPoints" << std::endl;
+    std::initializer_list<int> arr = {1, 2, 3};
+    scmales::Point<int, 3> p1(arr);
+    scmales::Point<int, 3> p2(arr);
+    std::cout << scmales::Point<int, 3>::equal(p1, p2) << std::endl;
+    if(p1 == p2)
+        std::cout << "Yes" << std::endl;
+    else
+        std::cout<< "None" << std::endl;
+    
     return 0;
 }
